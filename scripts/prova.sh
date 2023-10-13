9624e88dc7f2a4f04cc6d3fdf7259f209df3cc47
Daveonwave <davide.salaorni@polimi.it>
2023-10-13 14:23:51 +0200

    new branch: vdbse

--- scripts/prova.sh
+++ scripts/prova.sh
@@ -1,1 +1,6 @@
-python ernesto.py --models thevenin rc_thermal bolun
+python ernesto.py \
+  --battery_model thevenin \
+  --thermal_model rc_thermal \
+  --aging_model bolun \
+  --plot \
+  simulation
